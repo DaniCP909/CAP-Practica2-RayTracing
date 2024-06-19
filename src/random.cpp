2fcc46db2bfd7e368b8ebfcda522706bb3484b5c
DaniCP909 <d.corredorpadrino@gmail.com>
2024-06-19 10:55:22 +0200

    Add Project

---
+++ src/random.cpp
@@ -0,0 +1,23 @@
+#include "random.h"
+
+#include <cstdlib>
+
+inline float randomCap() {
+	return rand() / (RAND_MAX + 1.0f);
+}
+
+Vec3 randomNormalSphere() {
+	Vec3 p(2.0f * Vec3(randomCap(), randomCap(), randomCap()) - Vec3(1, 1, 1));
+	while (p.squared_length() >= 1.0) {
+		p = 2.0f * Vec3(randomCap(), randomCap(), randomCap()) - Vec3(1, 1, 1);
+	}
+	return p;
+}
+
+Vec3 randomNormalDisk() {
+	Vec3 p(2.0f * Vec3(randomCap(), randomCap(), 0) - Vec3(1, 1, 0));
+	while (dot(p, p) >= 1.0f) {
+		p = 2.0f * Vec3(randomCap(), randomCap(), 0) - Vec3(1, 1, 0);
+	}
+	return p;
+}
---
+++ src/random.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Vec3.h"
+
+float randomCap();
+Vec3 randomNormalSphere();
+Vec3 randomNormalDisk();
